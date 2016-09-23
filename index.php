95fb9eaed6ffbe7a74ea8a661567467634104560
m@rtlin2cz <martlin2cz@gmail.com>
2016-09-23 09:59:51 +0200

    Added hello world index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Hello!</title>
+	</head>
+	<body>
+		<h1>nanotube-cms</h1>
+		<p>This will be ASAP the <em>nanotup-cms</em>. Stay tuned!</p>
+	</body>
+</html>
